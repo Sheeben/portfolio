ec43afdd25b537d6d801e789f6b2758b3330215d
Sheeben <sheetalbdr8@gamil.com>
2024-11-08 12:31:32 +0530

    Initial commit: Portfolio

---
+++ src/scripts/script.js
@@ -0,0 +1,124 @@
+// Smooth scroll to sections when nav link is clicked
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const sectionId = this.getAttribute('href').substring(1);
+      document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
+
+ 
+
+  let circularProgress = document.querySelector(".circular-progress"),
+  progressValue = document.querySelector(".progress-value");
+
+let progressStartValue = 0,    
+  progressEndValue = 90,    
+  speed = 100;
+  
+let progress = setInterval(() => {
+  progressStartValue++;
+
+  progressValue.textContent = `${progressStartValue}%`
+  circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`
+
+  if(progressStartValue == progressEndValue){
+      clearInterval(progress);
+  }    
+}, speed);
+
+
+
+
+let circularProgresstwo = document.querySelector(".circular-progress2"),
+progressValuetwo = document.querySelector(".progress-value2");
+
+let progressStartValuetwo = 0,    
+progressEndValuetwo = 75,    
+speedtwo = 100;
+
+let progresstwo = setInterval(() => {
+progressStartValuetwo++;
+
+progressValuetwo.textContent = `${progressStartValuetwo}%`
+circularProgresstwo.style.background = `conic-gradient(#7d2ae8 ${progressStartValuetwo * 3.6}deg, #ededed 0deg)`
+
+if(progressStartValuetwo == progressEndValuetwo){
+    clearInterval(progresstwo);
+}    
+}, speedtwo);
+
+
+
+let circularProgressthree = document.querySelector(".circular-progress3"),
+progressValuethree = document.querySelector(".progress-value3");
+
+let progressStartValuethree = 0,    
+progressEndValuethree = 75,    
+speedthree = 100;
+
+let progressthree = setInterval(() => {
+progressStartValuethree++;
+
+progressValuethree.textContent = `${progressStartValuethree}%`
+circularProgressthree.style.background = `conic-gradient(#7d2ae8 ${progressStartValuethree * 3.6}deg, #ededed 0deg)`
+
+if(progressStartValuethree == progressEndValuethree){
+    clearInterval(progressthree);
+}    
+}, speedthree);
+
+
+
+let circularProgressfour = document.querySelector(".circular-progress4"),
+progressValuefour = document.querySelector(".progress-value4");
+
+let progressStartValuefour = 0,    
+progressEndValuefour = 85,    
+speedfour = 100;
+
+let progressfour = setInterval(() => {
+progressStartValuefour++;
+
+progressValuefour.textContent = `${progressStartValuefour}%`
+circularProgressfour.style.background = `conic-gradient(#7d2ae8 ${progressStartValuefour * 3.6}deg, #ededed 0deg)`
+
+if(progressStartValuefour == progressEndValuefour){
+    clearInterval(progressfour);
+}    
+}, speedfour);
+
+
+
+
+
+const titles = ["Full Stack Web Developer", "UI/UX Developer ", "MERN Stack Developer"];
+let index = 0;
+
+function changeTitle() {
+    const titleElement = document.getElementById("changing-title");
+    titleElement.textContent = titles[index];
+    index = (index + 1) % titles.length; // Cycle through the array
+}
+
+// Change title every 3 seconds (3000ms)
+setInterval(changeTitle, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
